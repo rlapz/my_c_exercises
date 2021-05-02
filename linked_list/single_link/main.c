d717c79e82c3fe6192c9fdd736bb723ec198c521
rLapz <arthurlapz@gmail.com>
2021-05-02 08:07:00 +0700

    modified:   linked_list/single_link/main.c

--- linked_list/single_link/main.c
+++ linked_list/single_link/main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include "list.h"
 
+void
+print_list(List **N)
+{
+	List *tmp = *N;
+	while (tmp != NULL) {
+		printf("%s ", (char*)tmp->data);
+		tmp = tmp->next;
+	}
+	puts("");
+}
 
 int
 main(void)
 {
 	List *N = new_list();
 
-	N = append_list(&N, (int8_t*)"hello ");
-	N = insert_list(&N, (int8_t*)"well ", 0);
-	N = append_list(&N, (int8_t*)"world ");
-	N = insert_list(&N, (int8_t*)"hmm ", 0);
+	N = append_list(&N, (char*)"hello ");
+	N = insert_list(&N, (char*)"well ", 0);
+	N = append_list(&N, (char*)"world ");
+	N = insert_list(&N, (char*)"hmm ", 0);
 
 	printf("size list: %lu\n", get_size_list(&N));
 	/*
 
 	print_list(&N);
-	N = insert_list(&N, (int8_t*)", ", 1);
+	N = insert_list(&N, (char*)", ", 1);
 	print_list(&N);
 	delete_last_list(&N);
 	*/
